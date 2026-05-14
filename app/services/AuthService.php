095a7986d12361fb01e633b05b32a63ade45df4b
Mbanzoulou <o2mhova@gmail.com>
2026-05-14 18:07:27 +0100

    created the authenticate function

--- app/services/AuthService.php
+++ app/services/AuthService.php
@@ -1,16 +1,18 @@
 <?php
 namespace App\Services;
 
+use App\Models\UserModel;
+
 class AuthService
 {
-    private $userModel;
+    private UserModel $userModel;
 
-    public function __construct($userModel)
+    public function __construct(UserModel $userModel)
     {
         $this->userModel = $userModel;
     }
 
-    public function authenticate($username, $password)
+    public function authenticate(string $username, string $password)
     {
         $user = $this->userModel->findByUsername($username);
         if ($user && password_verify($password, $user['password'])) {
