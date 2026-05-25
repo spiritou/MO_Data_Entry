c971ed6283d42213b72d7c5cbea3c61e3a2bc986
Mbanzoulou <o2mhova@gmail.com>
2026-05-25 01:27:39 +0100

    loginForm to login-form in my login js

--- public/js/login.js
+++ public/js/login.js
@@ -1,6 +1,6 @@
 /*This page will contain the JavaScript code for the login page. It will handle the form submission, validate the input, and send an AJAX request to the server to authenticate the user.*/
 
-document.getElementById('loginForm').addEventListener('submit', async function(event) {
+document.getElementById('login-form').addEventListener('submit', async function(event) {
     event.preventDefault(); // Prevent the default form submission
     
     // Get the input values
