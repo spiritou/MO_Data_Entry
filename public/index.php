9b3e21905f654ab92e17a06af645406c88e71929
Mbanzoulou <o2mhova@gmail.com>
2026-06-06 17:18:24 +0100

    router created successfully using the container

--- public/index.php
+++ public/index.php
@@ -2,7 +2,18 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../config/config.php';
+require_once __DIR__ . '/../routes/web.php';
 
 use App\Core\Router;
 use App\Core\Container;
 use PDO;
+
+$container = new Container();
+$router = $container->get(Router::class);
+
+//check if the router object is created successfully
+if ($router instanceof Router) {
+    echo "Router instance created successfully!";
+} else {
+    echo "Failed to create Router instance.";
+}
