31d57ce056a938caa95a84f0898b798f0cefefd2
Mbanzoulou <o2mhova@gmail.com>
2026-06-06 17:09:09 +0100

    moved the testing code into test php

--- public/index.php
+++ public/index.php
@@ -1,30 +1,8 @@
 <?php
-//i want to test the container class
-require_once __DIR__ .'/../vendor/autoload.php';
-require_once __DIR__ .'/../config/config.php';
 
-use App\Core\Container;
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../config/config.php';
+
 use App\Core\Router;
+use App\Core\Container;
 use PDO;
-
-$container = new Container();
-$router = new Router();
-$router->dispatch(); //this will run the router and check the current path and method
-$container->bind(PDO::class, function () {
-   return new PDO(
-    'mysql:host=' . DB_HOST . 
-    ';port=' . DB_PORT .
-    ';dbname=' . DB_DATABASE,
-    DB_USERNAME,
-    DB_PASSWORD
-   );
-});
-
-try {
-    $pdo = $container->get(PDO::class);
-    echo "PDO instance created successfully!";
-} catch (Exception $e) {
-    echo "Error: " . $e->getMessage();
-}
-
-
--- public/test.php
+++ public/test.php
@@ -1,5 +1,30 @@
 <?php
+//i want to test the container class
+require_once __DIR__ .'/../vendor/autoload.php';
+require_once __DIR__ .'/../config/config.php';
 
-//this is a test file to check if the config file is working properly
-require_once '../config/config.php';
-echo APP_URL;
+use App\Core\Container;
+use App\Core\Router;
+use PDO;
+
+$container = new Container();
+$router = new Router();
+$router->dispatch(); //this will run the router and check the current path and method
+$container->bind(PDO::class, function () {
+   return new PDO(
+    'mysql:host=' . DB_HOST . 
+    ';port=' . DB_PORT .
+    ';dbname=' . DB_DATABASE,
+    DB_USERNAME,
+    DB_PASSWORD
+   );
+});
+
+try {
+    $pdo = $container->get(PDO::class);
+    echo "PDO instance created successfully!";
+} catch (Exception $e) {
+    echo "Error: " . $e->getMessage();
+}
+
+
