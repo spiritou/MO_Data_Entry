73002b69047e9f3b06f19c8bbeec8e5f52f9ceea
Mbanzoulou <o2mhova@gmail.com>
2026-06-11 21:12:37 +0100

    creating routes

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../config/config.php';
-require_once __DIR__ . '/../routes/web.php';
+
 
 use App\Core\Router;
 use App\Core\Container;
@@ -10,6 +10,8 @@ use PDO;
 
 $container = new Container();
 $router = $container->get(Router::class);
+
+require_once __DIR__ . '/../routes/web.php';
 
 //check if the router object is created successfully
 if ($router instanceof Router) {
