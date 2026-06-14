77e45fc814edddf1c5789ac29d6ac67abfdac48b
Mbanzoulou <o2mhova@gmail.com>
2026-06-14 12:20:14 +0100

    changed MO_app to MO_APP

--- app/core/Router.php
+++ app/core/Router.php
@@ -19,7 +19,7 @@ class Router
 
         $method = $_SERVER['REQUEST_METHOD'];
         $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-        $path = str_replace('/MO_app/public', '', $path); //remove the /app/public part from the path
+        $path = str_replace('/MO_APP/public', '', $path); //remove the /app/public part from the path
         
         if(!isset($this->routes[$method])) {
             //if the route is not defined, we will return a 404 error
