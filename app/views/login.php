7a870fcd0340b1c33dc3e705470e2a9106d7eec7
Mbanzoulou <o2mhova@gmail.com>
2026-06-20 19:56:56 +0100

    change the path for css and js to be reachable from any device

--- app/views/login.php
+++ app/views/login.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Operations Tracker Login</title>
-    <link rel="stylesheet" href="<?= APP_URL ?>/css/login.css">
+    <link rel="stylesheet" href="css/login.css">
 </head>
 <body>
     <main class="login-page">
@@ -38,6 +38,6 @@
         </section>
     </main>
     <!-- called by login.js -->
-    <script src="<?= APP_URL ?>/js/login.js"></script>
+    <script src="js/login.js"></script>
 </body>
 </html>
