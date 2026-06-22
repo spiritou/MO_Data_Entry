cb492f0831c857e5a5c6d7bdab7f03135f685275
Mbanzoulou <o2mhova@gmail.com>
2026-06-22 23:36:58 +0100

    the ide does not seem to recognize  although index has a require once after creating a router objec

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+/* @var \App\Core\Router $router */
+
 $router->get('/', 'App\\Controllers\\AuthController@index');
 
 $router->post('/api/login', 'App\\Controllers\\AuthController@login');
