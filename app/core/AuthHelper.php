a6f0727bb486a7f351d9471a7861038c47bdeb2c
Mbanzoulou <o2mhova@gmail.com>
2026-07-04 17:27:38 +0100

    AuthHelper checking for the users role

--- app/core/AuthHelper.php
+++ app/core/AuthHelper.php
@@ -2,4 +2,7 @@
 //here will be constructed a helper class to handle role base acces controle
 
 class AuthHelper {
-    
+    public static function isAdmin(): bool {
+        return isset($_SESSION['role']) && $_SESSION['role'] === 'admin';
+    }
+}
