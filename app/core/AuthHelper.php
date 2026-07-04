d6e3a711abe62f620ebd243a806fc945191a9126
Mbanzoulou <o2mhova@gmail.com>
2026-07-04 17:28:25 +0100

    just added the namespace in AuthHelper

--- app/core/AuthHelper.php
+++ app/core/AuthHelper.php
@@ -1,6 +1,8 @@
 <?php
 //here will be constructed a helper class to handle role base acces controle
 
+namespace App\Core;
+
 class AuthHelper {
     public static function isAdmin(): bool {
         return isset($_SESSION['role']) && $_SESSION['role'] === 'admin';
