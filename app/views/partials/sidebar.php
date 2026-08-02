d956ab6f82ccd08c341699aa6fe83957787fde45
Mbanzoulou <o2mhova@gmail.com>
2026-08-02 23:17:27 +0100

    modified the spacing in the sidebar

--- app/views/partials/sidebar.php
+++ app/views/partials/sidebar.php
@@ -3,35 +3,37 @@ use App\Core\AuthHelper;
 ?>
 
 <aside class="sidebar">
-    <div class="logo">
-        <div class="logo-icon">
-            <i class="fa-solid fa-gear"></i>
+    <div class="sidebar-top">
+        <div class="logo">
+            <div class="logo-icon">
+                <i class="fa-solid fa-gear"></i>
+            </div>
+
+            <div class="logo-text">
+                <h2>MO Data Entry</h2>
+                <span>Maximo Optimized</span>
+            </div>
         </div>
 
-        <div class="logo-text">
-            <h2>MO Data Entry</h2>
-            <span>Maximo Optimized</span>
-        </div>
-    </div>
-
-    <nav class="menu">
-        <a href="#" class="menu-item active">
-            <i class="fa-solid fa-table-columns"></i>
-            <span>Dashboard</span>
-        </a>
-
-        <a href="#" class="menu-item">
-            <i class="fa-regular fa-file-lines"></i>
-            <span>Data Entry</span>
-        </a>
-
-        <?php if (AuthHelper::isAdmin()): ?>
-            <a href="#" class="menu-item">
-                <i class="fa-solid fa-users"></i>
-                <span>User Management</span>
+        <nav class="menu">
+            <a href="#" class="menu-item active">
+                <i class="fa-solid fa-table-columns"></i>
+                <span>Dashboard</span>
             </a>
-        <?php endif; ?>
-    </nav>
+
+            <a href="#" class="menu-item">
+                <i class="fa-regular fa-file-lines"></i>
+                <span>Data Entry</span>
+            </a>
+
+            <?php if (AuthHelper::isAdmin()): ?>
+                <a href="#" class="menu-item">
+                    <i class="fa-solid fa-users"></i>
+                    <span>User Management</span>
+                </a>
+            <?php endif; ?>
+        </nav>
+    </div>
 
     <div class="logout">
         <a href="#" id="logoutBtn">
