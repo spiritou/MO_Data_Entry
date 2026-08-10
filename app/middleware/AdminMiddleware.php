52361434165299617d669bb5ea5b05d657a42121
Mbanzoulou <o2mhova@gmail.com>
2026-08-10 14:22:03 +0100

    role access set up but will have to be refactored

--- app/middleware/AdminMiddleware.php
+++ app/middleware/AdminMiddleware.php
@@ -1,12 +1,12 @@
 <?php
 
 namespace App\Middleware;
-
-class AuthMiddleware
+use App\Core\AuthHelper;
+class AdminMiddleware
 {
     public static function handle(): void
     {
-        if ($_SESSION['role'] !== 'admin') {
+        if (!AuthHelper::isAdmin()) {
             http_response_code(403);
             exit('Access denied. You do not have permission to access this page.');
         }
