fde3bf9576797469097238facd9a3e9c2cdf9da4
Mbanzoulou <o2mhova@gmail.com>
2026-08-22 23:31:17 +0100

    adding user process

--- app/views/dashboard/index.php
+++ app/views/dashboard/index.php
@@ -4,5 +4,5 @@ $activePage = 'dashboard';
 ?>
 <main>
     <div> this is the dashboard </div>
-    <div>This page will be completed soon</div>
+    <div>This page will be completed later</div>
 </main>
